9fc88b7037248f1defd11fbabf5bad835c01e95f
JoseLuisGonzalezEscobar <jlgonzalez528@misena.edu.co>
2022-08-04 12:02:15 -0500

    modelos creados

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Continent extends Model
+class Region extends Model
 {
-    protected $table = "continents";
-    protected $primarykey = "continent_id";
-    public $timestamps = false;
     use HasFactory;
 }
